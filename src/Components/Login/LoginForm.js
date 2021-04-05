bdd848016172aca6ce0e14cdc752c5a91d2533fe
Daniel <daniel.n10@hotmail.com>
2021-04-04 22:44:06 -0300

    Create routes for the login page

---
+++ src/Components/Login/LoginForm.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    fetch(`https://dogsapi.origamid.dev/json/jwt-auth/v1/token`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username,
+        password
+      })
+    }).then(response => response.json()).then(json => console.log(json))
+  }
+
+  return (
+    <section>
+      Login Form
+      <h1>Login</h1>
+      <form action="" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={username}
+          onChange={({target}) => setUsername(target.value)}
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={({target}) => setPassword(target.value)}
+        />
+        <button>Entrar</button>
+      </form>
+      <Link to="/login/criar">Cadastro</Link>
+    </section>
+  )
+};
+
+export default LoginForm;
