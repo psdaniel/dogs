d8f28d0a25d72d98e8b79837c608c6d24f52580a
Daniel <daniel.n10@hotmail.com>
2021-04-05 07:48:44 -0300

    Create Button and Input

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
 
 const LoginForm = () => {
   const [username, setUsername] = useState('');
@@ -24,17 +26,9 @@ const LoginForm = () => {
       Login Form
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={username}
-          onChange={({target}) => setUsername(target.value)}
-        />
-        <input
-          type="password"
-          value={password}
-          onChange={({target}) => setPassword(target.value)}
-        />
-        <button>Entrar</button>
+        <Input label="Usuário" type="text" name="username" />
+        <Input label="Senha" type="password" name="password" />
+        <Button>Entrar</Button>
       </form>
       <Link to="/login/criar">Cadastro</Link>
     </section>
