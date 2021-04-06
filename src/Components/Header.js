b83462ba1eafa72213ccc96d05553ed97a875d98
Daniel <daniel.n10@hotmail.com>
2021-04-05 22:38:47 -0300

    Create userContext

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
-import {ReactComponent as Dogs} from '../Assets/dogs.svg'
+import {ReactComponent as Dogs} from '../Assets/dogs.svg';
+import { UserContext } from '../UserContext';
 
 const Header = () => {
+  const { data } = useContext(UserContext);
+  console.log(data)
+
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`} aria-label="Dogs - Home">
         <Link className={styles.logo} to="/">
           <Dogs />
         </Link>
-        <Link className={styles.login} to="/login">Login / Criar</Link>
+        {data ? (
+          <Link className={styles.login} to="/conta">
+            {data.nome}
+          </Link>
+        ) : (
+          <Link className={styles.login} to="/login">
+            Login / Criar
+          </Link>
+        )}
       </nav>
     </header>
   )
---
+++ src/UserContext.js
@@ -0,0 +1,34 @@
+import { getSuggestedQuery } from '@testing-library/dom';
+import React, { createContext, useState } from 'react';
+import { TOKEN_POST, USER_GET } from './api';
+
+export const UserContext = createContext();
+
+export const UserStorage = ({ children }) => {
+  const [data, setData] = useState(null);
+  const [login, setLogin] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  async function getUser(token) {
+    const { url, options } = USER_GET(token);
+    const response = await fetch(url, options);
+    const json = await response.json();
+    setData(json);
+    setLogin(true);
+  }
+
+  async function userLogin(username, password) {
+    const { url, options } = TOKEN_POST({ username, password });
+    const tokenRes = await fetch(url, options);
+    const { token } = await tokenRes.json();
+    window.localStorage.setItem('token', token);
+    getUser()
+  }
+
+  return (
+    <UserContext.Provider value={{ userLogin, data }}>
+      {children}
+    </UserContext.Provider>
+    )
+};
