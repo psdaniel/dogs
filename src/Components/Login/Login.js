e08b8bf7dfdbdb94010cff5ec016bddd2f21da77
Daniel <daniel.n10@hotmail.com>
2021-04-06 21:21:39 -0300

    Styles login page

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -5,6 +5,7 @@ import LoginCreate from './LoginCreate';
 import LoginPasswordLost from './LoginPasswordLost';
 import LoginPasswordReset from './LoginPasswordReset';
 import { UserContext } from '../../UserContext';
+import styles from './Login.module.css';
 
 const Login = () => {
   const { login } = useContext(UserContext);
@@ -12,14 +13,16 @@ const Login = () => {
   if (login === true) return <Navigate to="/conta" />
 
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="criar" element={<LoginCreate />} />
-        <Route path="perdeu" element={<LoginPasswordLost />} />
-        <Route path="resetar" element={<LoginPasswordReset />} />
-      </Routes>
-    </div>
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="criar" element={<LoginCreate />} />
+          <Route path="perdeu" element={<LoginPasswordLost />} />
+          <Route path="resetar" element={<LoginPasswordReset />} />
+        </Routes>
+      </div>
+    </section>
   )
 };
 
