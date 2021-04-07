5d7477cb5d60e58b869546ff60aa4bef9a44626c
Daniel <daniel.n10@hotmail.com>
2021-04-07 08:24:41 -0300

    Create protected route

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
 import Footer from './Components/Footer';
 import Header from './Components/Header';
+import ProtectedRoute from './Components/Helper/ProtectedRoute';
 import Home from './Components/Home';
 import Login from './Components/Login/Login';
+import User from './Components/User/User';
 import { UserStorage } from './UserContext';
 
 const App = () => {
@@ -15,7 +17,8 @@ const App = () => {
           <Header />
           <Routes>
             <Route path="/" element={<Home />}/>
-            <Route path="/login/*" element={<Login />}/>
+            <Route path="login/*" element={<Login />}/>
+            <ProtectedRoute path="conta/*" element={<User />}/>
           </Routes>
           <Footer />
         </UserStorage>
