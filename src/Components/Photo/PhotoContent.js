351e6c1853403ae8d10e73f13b9468cf798a2514
Daniel <daniel.n10@hotmail.com>
2021-04-08 20:34:43 -0300

    Create photo delete

--- src/Components/Photo/PhotoContent.js
+++ src/Components/Photo/PhotoContent.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { UserContext } from '../../UserContext';
 import PhotoComments from './PhotoComments';
 import styles from './PhotoContent.module.css';
+import PhotoDelete from './PhotoDelete';
 
 const PhotoContent = ({ data }) => {
+  const user = useContext(UserContext);
   const {photo, comments} = data;
 
   return (
@@ -13,7 +16,11 @@ const PhotoContent = ({ data }) => {
         <div className={styles.details}>
           <div>
             <p className={styles.author}>
-              <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+              {user.data && user.data.username === photo.author ? (
+                <PhotoDelete id={photo.id} />
+              ) : (
+                <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+              )}
               <span className={styles.visualizacoes}>{photo.acessos}</span>
             </p>
             <h1 className="title">
---
+++ src/Components/Photo/PhotoDelete.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { PHOTO_DELETE } from '../../api';
+import useFetch from '../../Hooks/useFetch';
+import styles from './PhotoDelete.module.css';
+
+const PhotoDelete = ({ id }) => {
+  const { loading, request } = useFetch();
+
+  async function handleClick() {
+    const confirm = window.confirm('Tem certeza que deseja deletar?');
+    if (confirm) {
+      const { url, options } = PHOTO_DELETE(id);
+      const { response } = await request(url, options);
+      if (response.ok) window.location.reload();
+    }
+  }
+
+  return (
+    <>
+      {loading ? (
+        <button className={styles.delete} disabled>Deletar</button>
+        ) : (
+        <button onClick={handleClick} className={styles.delete}>Deletar</button>
+        )}
+    </>
+  )
+}
+
+export default PhotoDelete;
