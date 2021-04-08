3577ce0edbf46e6356d46de61a26fcfd341d887f
Daniel <daniel.n10@hotmail.com>
2021-04-08 20:04:50 -0300

    Styles comments section

--- src/Components/Photo/PhotoComments.js
+++ src/Components/Photo/PhotoComments.js
@@ -1,15 +1,20 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import { UserContext } from '../../UserContext';
 import PhotoCommentsForm from './PhotoCommentsForm';
 import styles from './PhotoComments.module.css';
 
 const PhotoComments = (props) => {
-  const [comments, setComments] = useState(() => props.comments)
+  const [comments, setComments] = useState(() => props.comments);
+  const commentsSection = useRef(null);
   const { login } = useContext(UserContext);
+
+  useEffect(() => {
+    commentsSection.current.scrollTop = commentsSection.current.scrollHeight;
+  }, [comments])
 
   return (
     <>
-      <ul className={styles.comments}>
+      <ul ref={commentsSection} className={styles.comments}>
         {comments.map(comment => (<li key={comment.comment_ID}>
           <b>{comment.comment_author}: </b>
           <span>{comment.comment_content}</span>
