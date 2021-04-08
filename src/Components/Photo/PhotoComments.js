35aec530f33b4f2cd5c473cf4267ca15f713e783
Daniel <daniel.n10@hotmail.com>
2021-04-08 19:51:27 -0300

    Create comment post

--- src/Components/Photo/PhotoComments.js
+++ src/Components/Photo/PhotoComments.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { UserContext } from '../../UserContext';
+import PhotoCommentsForm from './PhotoCommentsForm';
+import styles from './PhotoComments.module.css';
 
-const PhotoComments = () => {
+const PhotoComments = (props) => {
+  const [comments, setComments] = useState(() => props.comments)
+  const { login } = useContext(UserContext);
+
   return (
-    <div>
-
-    </div>
+    <>
+      <ul className={styles.comments}>
+        {comments.map(comment => (<li key={comment.comment_ID}>
+          <b>{comment.comment_author}: </b>
+          <span>{comment.comment_content}</span>
+        </li>))}
+      </ul>
+      {login && <PhotoCommentsForm id={props.id} setComments={setComments} />}
+    </>
   )
 }
 
