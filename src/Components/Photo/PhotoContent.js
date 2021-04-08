c185899a49dcb21237876c48d60d87f91f20f433
Daniel <daniel.n10@hotmail.com>
2021-04-08 19:22:12 -0300

    Styles modal photo and create outsideClick logic

--- src/Components/Photo/PhotoContent.js
+++ src/Components/Photo/PhotoContent.js
@@ -12,7 +12,7 @@ const PhotoContent = ({ data }) => {
         <img src={photo.src} alt={photo.title} />
         <div className={styles.details}>
           <div>
-            <p>
+            <p className={styles.author}>
               <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
               <span className={styles.visualizacoes}>{photo.acessos}</span>
             </p>
