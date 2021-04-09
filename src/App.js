4cf092638c00a15599a9991e9300acb657871ea9
Daniel <daniel.n10@hotmail.com>
2021-04-09 09:18:43 -0300

    Create user profile

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Login from './Components/Login/Login';
 import User from './Components/User/User';
 import Photo from './Components/Photo/Photo';
 import { UserStorage } from './UserContext';
+import UserProfile from './Components/User/UserProfile';
 
 const App = () => {
   return (
@@ -21,6 +22,7 @@ const App = () => {
             <Route path="login/*" element={<Login />}/>
             <ProtectedRoute path="conta/*" element={<User />}/>
             <Route path="foto/:id" element={<Photo />} />
+            <Route path="perfil/:user" element={<UserProfile />} />
           </Routes>
           <Footer />
         </UserStorage>
