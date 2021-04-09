f981c3dbd702ea3bfdc7375a74beb819b5d6ee68
Daniel <daniel.n10@hotmail.com>
2021-04-08 21:03:07 -0300

    Create image skeleton

--- src/Components/Photo/PhotoContent.js
+++ src/Components/Photo/PhotoContent.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { UserContext } from '../../UserContext';
+import Image from '../Helper/Image';
 import PhotoComments from './PhotoComments';
 import styles from './PhotoContent.module.css';
 import PhotoDelete from './PhotoDelete';
@@ -12,7 +13,7 @@ const PhotoContent = ({ data }) => {
   return (
     <div className={styles.photo}>
       <div className={styles.img}>
-        <img src={photo.src} alt={photo.title} />
+        <Image src={photo.src} alt={photo.title} />
         <div className={styles.details}>
           <div>
             <p className={styles.author}>
