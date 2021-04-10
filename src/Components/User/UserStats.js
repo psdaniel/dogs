b537b3c66495988ebd1ab9742f7ac324c24c1f62
Daniel <daniel.n10@hotmail.com>
2021-04-09 21:17:08 -0300

    Add lazy and suspense to stats lib

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,10 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { lazy, Suspense, useEffect } from 'react';
 import Head from '../Helper/Head';
 import useFetch from '../../Hooks/useFetch';
 import { STATS_GET } from '../../api';
 import Loading from '../Helper/Loading';
 import Error from '../Helper/Error';
-import UserStatsGraphs from './UserStatsGraphs';
+const UserStatsGraphs = lazy(() => import('./UserStatsGraphs'));
 
 const UserStats = () => {
   const { data, error, loading, request} = useFetch();
@@ -21,10 +21,10 @@ const UserStats = () => {
   if(error) return <Error error={error} />
   if(data)
     return (
-      <div>
+      <Suspense fallback={<div></div>}>
         <Head title="Estatísticas" />
         <UserStatsGraphs data={data} />
-      </div>
+      </Suspense>
     )
   else return null;
 };
