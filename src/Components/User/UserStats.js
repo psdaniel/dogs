9251256d28b7d9219c796eba49add4b94d9195ff
Daniel <daniel.n10@hotmail.com>
2021-04-09 21:13:55 -0300

    Styles user stats page

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Head from '../Helper/Head';
+import useFetch from '../../Hooks/useFetch';
+import { STATS_GET } from '../../api';
+import Loading from '../Helper/Loading';
+import Error from '../Helper/Error';
+import UserStatsGraphs from './UserStatsGraphs';
 
 const UserStats = () => {
-  return (
-    <div>
-      <Head title="Estatísticas" />
-      Estatísticas
-    </div>
-  )
+  const { data, error, loading, request} = useFetch();
+
+  useEffect(() => {
+    async function getData() {
+      const {url, options } = STATS_GET();
+      await request(url, options)
+    }
+    getData();
+  }, [request]);
+
+  if(loading) return <Loading />
+  if(error) return <Error error={error} />
+  if(data)
+    return (
+      <div>
+        <Head title="Estatísticas" />
+        <UserStatsGraphs data={data} />
+      </div>
+    )
+  else return null;
 };
 
 export default UserStats;
