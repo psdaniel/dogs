c0a88a61035db14bb748c39cf3ca712caaa54f40
Daniel <daniel.n10@hotmail.com>
2021-04-09 21:25:10 -0300

    Some adjusts

--- src/Components/Login/LoginPasswordReset.js
+++ src/Components/Login/LoginPasswordReset.js
@@ -37,7 +37,7 @@ const LoginPasswordReset = () => {
   }
 
   return (
-    <div>
+    <section className="animeLeft">
       <Head title="Resete a senha" />
       <h1 className="title">Resete a Senha</h1>
       <form onSubmit={handleSubmit}>
@@ -49,7 +49,7 @@ const LoginPasswordReset = () => {
           )}
       </form>
       <Error error={error} />
-    </div>
+    </section>
   )
 };
 
